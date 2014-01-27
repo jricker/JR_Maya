4bd16c6248c3043004b2a33592814ed4a00cbb44
jricker <james.ricker@gmail.com>
2014-01-27 17:22:28 +0000

    updates to tool class

--- JR_tool_class.py
+++ JR_tool_class.py
@@ -91,10 +91,10 @@ class Tools(Selection, DraggerTool, Attributes, Materials):
 		X = self.getSelection() # adds selection to a variable so we can deselect it later while still using it for the Dragger command.
 		if self.getType(0) == 'face':
 			cmds.polyExtrudeFacet( X, constructionHistory = 1, keepFacesTogether = 1)
-			Attribute.setAttributes( attrs = [('Thickness', '.thickness'), ('Z Translate', '.localTranslateZ'), ('Local Scale', '.localScale'), ('Division', '.divisions')] )
+			Attribute.setAttributes( attrs = [('Thickness', '.thickness'), ('Offset', '.offset'), ('Division', '.divisions'), ('Z Translate', '.localTranslateZ')] )
 		elif self.getType(0) == 'edge':
 			cmds.polyExtrudeEdge( X, constructionHistory = 1, keepFacesTogether = 1)
-			Attribute.setAttributes( attrs = [('Thickness', '.thickness'), ('Z Translate', '.localTranslateZ'), ('Local Scale', '.localScale'), ('Division', '.divisions')] )
+			Attribute.setAttributes( attrs = [('Thickness', '.thickness'), ('Offset', '.offset'), ('Division', '.divisions'), ('Z Translate', '.localTranslateZ')] )
 		else:
 			pass
 		#cmds.select( deselect = 1 ) # removes selection from the face initially extruded, to allow for rotation and translation of new extruded face
